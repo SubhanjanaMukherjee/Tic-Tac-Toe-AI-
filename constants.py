2aaa0fd02fd2aa62f7eb1c38c82fa61d91dbd481
Subhanjana Mukherjee <73611642+SubhanjanaMukherjee@users.noreply.github.com>
2022-07-03 13:36:39 +0530

    Add files via upload
    Game Instructions :
    press 'g' to change gamemode (pvp or ai)
    press '0' to change ai level to 0 (random)
    press '1' to change ai level to 1 (impossible)
    press 'r' to restart the game

---
+++ constants.py
@@ -0,0 +1,23 @@
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+
+SQSIZE = WIDTH // COLS
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE//4
+
+OFFSET = 50 # distance between begining and end of line from board lines
+
+#COLOUR
+BG_COLOR = (106,65,196)
+LINE_COLOR = (59,36,110)
+CIRC_COLOR = (239 ,231,200)
+CROSS_COLOR = (51,50,50)
+WIN_COLOR = (235,48,35)
+
